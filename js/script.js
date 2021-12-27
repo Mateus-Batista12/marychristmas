ca0f521dc5be2d0fdc9fe61fc17ed61fe68a21c0
Mateus-Batista12 <mateus.batista12f@gmail.com>
2021-12-27 12:17:04 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+    menu.classList.toggle('fa-times'); //quando clicar sobre o botao muda o icone para X;
+    navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times'); 
+    navbar.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.home .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
